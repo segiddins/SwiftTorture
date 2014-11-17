37490277768faae4c3f5be8829dd6a114d1a021a
Samuel E. Giddins <segiddins@segiddins.me>
2014-11-17 11:58:44 -0800

    add swift

---
+++ SwiftTorture/View.swift
@@ -0,0 +1,13 @@
+//
+//  View.swift
+//  SwiftTorture
+//
+//  Created by Samuel E. Giddins on 11/17/14.
+//  Copyright (c) 2014 Samuel E. Giddins. All rights reserved.
+//
+
+import UIKit
+
+class View: UIView {
+
+}
